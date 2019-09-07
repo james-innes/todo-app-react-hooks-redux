b6f25e5df7854a92ed86f48ca189ae24e1f332c9
Gual Casas <gualcm@gmail.com>
2019-09-06 19:10:17 -0700

    woops

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,10 +1,11 @@
 import { configureStore, getDefaultMiddleware } from 'redux-starter-kit'
 import logger from 'redux-logger'
 import { reduxBatch } from '@manaflair/redux-batch'
-import preloadedState from './preloadedState';
 import { loadState, saveState } from './localStorage'
 import reducer from './todosSlice';
 import { throttle } from 'lodash';
+
+const persistedState = loadState();
 
 const store = configureStore({
     reducer: {
